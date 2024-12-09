b36e4f2625c18b237584497eb86195786ad478cf
Antonin Lopez <antoninlopez85@gmail.com>
2024-12-09 15:07:52 +0100

    petit changement dans CreerFichier

--- src/modele/CreerFichier.java
+++ src/modele/CreerFichier.java
@@ -1,10 +1,9 @@
-package test;
+package modele;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import modele.*;
 import modele.option.*;
 
 public class CreerFichier
