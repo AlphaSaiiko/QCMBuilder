63bb12dbcbaa05873b9e70ec06eba83de257fbcd
Nahel <nahel.kochat@laposte.net>
2024-12-09 13:37:09 +0100

    Creation evaluation java

---
+++ src/modele/Evaluation.java
@@ -0,0 +1,81 @@
+package modele;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Evaluation 
+{
+	/*
+	 *  +------------+
+	 *  | ATTRIBUTS  |
+	 *  +------------+
+	 */
+
+	private Ressource ressource;
+	private List<Question> listeQuestions;
+
+	private String lienEval; // Lien de l'évaluation à mettre sur l'intranet
+
+
+	/*
+	 *  +--------------+
+	 *  | CONSTRUCTEUR |
+	 *  +--------------+
+	 */
+
+	public Evaluation(Ressource ressource)
+	{
+		this.ressource = ressource;
+		this.listeQuestions = new ArrayList<Question>();
+	}
+
+	public boolean ajouterQuestion(Question qt)
+	{
+		if (this.listeQuestions != null && qt != null)
+		{
+			this.listeQuestions.add(qt);
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 *  +----------+
+	 *  | GETTEURS |
+	 *  +----------+
+	 */
+
+	
+
+	public Ressource getRessource() 
+	{
+		return ressource;
+	}
+
+	public List<Question> getListeQuestions() 
+	{
+		return listeQuestions;
+	}
+
+	public String getLienEval() 
+	{
+		return lienEval;
+	}
+
+
+	/*
+	 *  +----------+
+	 *  | SETTEURS |
+	 *  +----------+
+	 */
+
+	public void setRessource(Ressource ressource) {
+		this.ressource = ressource;
+	}
+
+	public void setLienEval(String lienEval) {
+		this.lienEval = lienEval;
+	}
+
+	 
+}
