f8135417fcaaa7049f90217ea6fa754f4869de1d
Nahel <nahel.kochat@laposte.net>
2024-12-09 14:10:15 +0100

    Debut controleur

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -1,6 +1,20 @@
 package controleur;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import modele.Evaluation;
+import vue.Accueil;
+
 public class Controleur
 {
 
+	private static List<Evaluation> listEval;
+	private static Accueil    acc ;
+
+	public Controleur()
+	{
+		Controleur.acc      = new Accueil();
+		Controleur.listEval = new ArrayList<Evaluation>();
+	}
 }
