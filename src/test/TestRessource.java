78df5769fca55823ac23a90709cb1b5c9ee6870c
Laparre Quentin <quentin.laparre@gmail.com>
2024-12-09 10:43:40 +0100

    ajout test

---
+++ src/test/TestRessource.java
@@ -0,0 +1,29 @@
+package test;
+
+import java.io.File;
+
+import modele.*;
+
+public class TestRessource
+{
+	public static void main(String[] args) {
+		Ressource tmpRes;
+
+		tmpRes = new Ressource("ressource1");
+
+		// Création d'un objet File représentant le répertoire
+		File repertoire = new File("../../lib/ressources/"+tmpRes.getNom());
+
+		// Vérification et création du répertoire
+		if (!repertoire.exists()) {
+			boolean estCree = repertoire.mkdirs(); // mkdir() pour un seul niveau, mkdirs() pour plusieurs niveaux
+			if (estCree) {
+				System.out.println("Répertoire créé avec succès !");
+			} else {
+				System.out.println("Échec de la création du répertoire.");
+			}
+		} else {
+			System.out.println("Le répertoire existe déjà.");
+		}
+	}
+}
