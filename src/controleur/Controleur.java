3f2259eca041627c8f79a466a1fa149f92874daf
Nahel <nahel.kochat@laposte.net>
2024-12-12 09:13:03 +0100

    Ajout chargement des réponses (en commentaire)

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -1,20 +1,11 @@
 package controleur;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 import java.util.stream.Stream;
-import modele.Evaluation;
-import modele.Notion;
-import modele.Question;
-import modele.Ressource;
+import modele.*;
+import modele.option.Option;
 import vue.Accueil;
 
 public class Controleur
@@ -28,54 +19,56 @@ public class Controleur
 		Controleur.listEval = new ArrayList<>();
 	}
 
-    public static void chargerRessourcesEtNotion()
+	public static void chargerRessourcesEtNotion()
 	{
-        String resourcesPath = "lib/ressources";
-        try (Stream<Path> paths = Files.walk(Paths.get(resourcesPath)))
+		String resourcesPath = "lib/ressources";
+		try (Stream<Path> paths = Files.walk(Paths.get(resourcesPath)))
 		{
-            paths.filter(Files::isDirectory).forEach(path ->
+			paths.filter(Files::isDirectory).forEach(path ->
 			{
-                File dir = path.toFile();
-                File parent = dir.getParentFile();
+				File dir = path.toFile();
+				File parent = dir.getParentFile();
 
-                // Si le répertoire actuel est une ressource
-                if (parent != null && parent.getName().equals("ressources"))
+				// Si le répertoire actuel est une ressource
+				if (parent != null && parent.getName().equals("ressources"))
 				{
-                    System.out.println("Ressource trouvée : " + dir.getName());
-                    Ressource.creerRessource(dir.getName());
-                }
-                // Si le répertoire parent est une ressource
-                else if (parent != null && parent.getParentFile() != null && parent.getParentFile().getName().equals("ressources"))
+					System.out.println("Ressource trouvée : " + dir.getName());
+					Ressource.creerRessource(dir.getName());
+				}
+				// Si le répertoire parent est une ressource
+				else if (parent != null && parent.getParentFile() != null && parent.getParentFile().getName().equals("ressources"))
 				{
-                    Ressource ressource = Ressource.creerRessource(parent.getName());
-                    if (ressource == null)
+					Ressource ressource = Ressource.creerRessource(parent.getName());
+					if (ressource == null)
 					{
-                        System.out.println("Ressource non trouvée pour la notion, création d'une nouvelle ressource : " + parent.getName());
-                        Ressource.creerRessource(parent.getName());
-                        ressource = Ressource.creerRessource(parent.getName());
-                    }
+						System.out.println("Ressource non trouvée pour la notion, création d'une nouvelle ressource : " + parent.getName());
+						Ressource.creerRessource(parent.getName());
+						ressource = Ressource.creerRessource(parent.getName());
+					}
 
-                    // Création de la notion
-                    Notion notion = Notion.creerNotion(dir.getName(), ressource);
-                    System.out.println("Notion créée : " + notion.getNom() + " pour la ressource : " + ressource.getNom());
+					// Création de la notion
+					Notion notion = Notion.creerNotion(dir.getName(), ressource);
+					System.out.println("Notion créée : " + notion.getNom() + " pour la ressource : " + ressource.getNom());
 
 					Controleur.chargerQuestion(notion, dir);
 
-                    // Ajout de la notion à la ressource
-                    System.out.println("Notion ajoutée à la ressource : " + ressource.getNom());
-                }
-            });
-        }
+					// Ajout de la notion à la ressource
+					System.out.println("Notion ajoutée à la ressource : " + ressource.getNom());
+				}
+			});
+		}
 		catch (IOException e)
 		{
-            System.err.println("Erreur lors du chargement des ressources et des notions : " + e.getMessage());
-        }
-    }
+			System.err.println("Erreur lors du chargement des ressources et des notions : " + e.getMessage());
+		}
+	}
 
 	public static void chargerQuestion(Notion notion, File dir)
 	{
 		//ArrayList<Question> tmplst = new ArrayList<Question>();
 		
+		// à gérer : les réponses
+
 		int cpt = 0;
 		// Boucle pour chaque question
 		for (File dossier : dir.listFiles())
@@ -92,13 +85,34 @@ public class Controleur
 
 				Object[] ligne = ligneQuestion.split("\t");
 
-				String type     = String.valueOf(ligne[0]);
+				String type	 = String.valueOf(ligne[0]);
 				String intitule = String.valueOf(ligne[1]);
-				int nbPoints    = Integer.valueOf(String.valueOf(ligne[2]));
-				int temps       = Integer.valueOf(String.valueOf(ligne[3]));
+				int nbPoints	= Integer.valueOf(String.valueOf(ligne[2]));
+				int temps	   = Integer.valueOf(String.valueOf(ligne[3]));
 				int difficulte  = Integer.valueOf(String.valueOf(ligne[4]));
 				
 				Question tmp = new Question(type, intitule, nbPoints, temps, difficulte, notion); 
+
+/*				// Chargement des réponses
+				while (sc.hasNextLine())
+				{
+					ligneQuestion = sc.nextLine();
+
+					ligne = ligneQuestion.split("\t");
+
+					String  intituleR  = String .valueOf(ligne[2]); // ligne[0] = Id,  ligne[1] = type
+					boolean estReponse = Boolean.valueOf(String.valueOf(ligne[3]));
+
+					switch (type) 
+					{
+						//case () Différents types à gérer, mais besoin de Fichier.java mis à jour
+					
+						default -> tmp.ajouterOption(new Option(type, intituleR, estReponse, tmp));
+					}
+					
+					
+				}*/
+
 				notion.ajouterQuestion(tmp);
 				
 				//tmplst.add(tmp);
@@ -121,7 +135,7 @@ public class Controleur
 	public static void main(String[] args)
 	{
 		Controleur controleur = new Controleur();
-		Controleur.acc      = new Accueil();
+		Controleur.acc	  = new Accueil();
 		controleur.chargerRessourcesEtNotion();
 	}
 }
