a3eec879f7b5a449c495f21bc939d89658c6f579
Tatanelprr <ethan.lepareur@gmail.com>
2024-12-12 14:19:54 +0100

    Update question files to change QAE to QCMRM format and adjust parameters; add new question files for resources

--- src/vue/QuestionLiaison.java
+++ src/vue/QuestionLiaison.java
@@ -105,6 +105,7 @@ public class QuestionLiaison extends JFrame
 		// Afficher la fenêtre
 		setTitle("Question Réponse Liaison");
 		setSize(800, 600);
+        setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
