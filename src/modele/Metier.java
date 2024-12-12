32e7c2d6a30c759ecc89c6008f7a56fffcbdd63c
Nahel <nahel.kochat@laposte.net>
2024-12-12 14:29:53 +0100

    classe metier

---
+++ src/modele/Metier.java
@@ -0,0 +1,37 @@
+package modele;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import controleur.Controleur;
+
+public class Metier 
+{
+	
+    /*
+     * +------------+
+     * | Attributs  |
+     * +------------+
+     */
+
+	private Controleur controleur;
+
+	private static List<Evaluation> listEval;
+
+    private static List<Ressource>  listRessource;
+
+
+    /*
+     * +--------------+
+     * | Constructeur |
+     * +--------------+
+     */
+
+	public Metier(Controleur controleur)
+	{
+		this.controleur = controleur;
+		Metier.listEval      = new ArrayList<Evaluation>();
+		Metier.listRessource = new ArrayList<Ressource>();
+	}
+
+}
