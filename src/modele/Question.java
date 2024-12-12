b5878ce5c0a096ec493c2f9bb5cc80c82830116d
Tatanelprr <ethan.lepareur@gmail.com>
2024-12-12 11:26:38 +0100

    Refactor Question class for improved readability and consistency; update formatting and comments

--- src/modele/Question.java
+++ src/modele/Question.java
@@ -2,165 +2,186 @@ package modele;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.lang.model.util.ElementScanner14;
-
 import modele.option.IOption;
 
 public class Question
 {
-	/*
-	 *  +------------+
-	 *  | PARAMETRES |
-	 *  +------------+
-	 */
-	private String type    ;
-	private String intitule;
+    /*
+     * +------------+
+     * | PARAMETRES |
+     * +------------+
+     */
+    private String type;
+    private String intitule;
+    private int nbPoints;
+    private int temps;
+    private int difficulte;
+    public int numQuestion;
+    private List<String> complements; // Les pièces jointes ou petites images
+    private List<IOption> ensOptions;
+    private Notion notion;
 
-	private int nbPoints  ;
-	private int temps     ;
-	private int difficulte;
-	public int numQuestion;
+    /*
+     * +--------------+
+     * | CONSTRUCTEUR |
+     * +--------------+
+     */
+    public static Question creerQuestion(int nbPoints, int temps, Notion notion, int difficulte, String type)
+    {
+        Question question = new Question(nbPoints, temps, notion, difficulte, type);
+        if (question != null)
+        {
+            System.out.println("La question a été créée avec succès.");
+        }
+        else
+        {
+            System.out.println("La question n'a pas pu être créée.");
+        }
+        return question;
+    }
 
-	private List<String> complements; // Les pièces jointes ou petites images
+    private Question(int nbPoints, int temps, Notion notion, int difficulte, String type)
+    {
+        this.nbPoints = nbPoints;
+        this.temps = temps;
+        this.notion = notion;
+        this.difficulte = difficulte;
+        this.type = type;
+        this.numQuestion = notion.getNbQuestion();
+        this.complements = new ArrayList<>();
+        this.ensOptions = new ArrayList<>();
+        this.creerFichierQuestion();
+        notion.ajouterQuestion(this);
+    }
 
-	private List<IOption> ensOptions;
+    /*
+     * +---------+
+     * | GETTERS |
+     * +---------+
+     */
+    public String getType()
+    {
+        return type;
+    }
 
-	private Notion notion;
+    public String getIntitule()
+    {
+        return intitule;
+    }
 
-	
-	/*
-	 *  +--------------+
-	 *  | CONSTRUCTEUR |
-	 *  +--------------+
-	 */
+    public int getNbPoints()
+    {
+        return nbPoints;
+    }
 
-	public static Question creerQuestion(int nbPoints, int temps, Notion notion, int difficulte, String type)
-	{
-		Question question = new Question(nbPoints, temps, notion, difficulte, type);
-		if (question != null)
-		{
-			System.out.println("La question a été créée avec succès.");
-		}
-		else
-		{
-			System.out.println("La question n'a pas pu être créée.");
-		}
-		return question;
-	}
-	
-	private Question(int nbPoints, int temps, Notion notion, int difficulte, String type )
-	{
-		this.nbPoints    = nbPoints   ;
-		this.temps       = temps      ;
-		this.notion      = notion     ;
-		this.difficulte  = difficulte ;
-		this.type        = type       ;
+    public int getTemps()
+    {
+        return temps;
+    }
 
-		this.numQuestion = notion.getNbQuestion();
+    public int getDifficulte()
+    {
+        return difficulte;
+    }
 
-		this.complements = new ArrayList<String>();
-		this.ensOptions  = new ArrayList<IOption>();
+    public Notion getNotion()
+    {
+        return notion;
+    }
 
-		this.creerFichierQuestion();
+    public List<String> getComplements()
+    {
+        return complements;
+    }
 
-		notion.ajouterQuestion(this);
+    public IOption getOptions(int ind)
+    {
+        return ensOptions.get(ind);
+    }
 
-	}
+    public List<IOption> getEnsOptions()
+    {
+        return ensOptions;
+    }
 
+    public int getNumQuestion()
+    {
+        return this.numQuestion;
+    }
 
-	/*
-	 *  +----------+
-	 *  | GETTEURS |
-	 *  +----------+
-	 */
+    /*
+     * +---------+
+     * | SETTERS |
+     * +---------+
+     */
+    public void setType(String type)
+    {
+        this.type = type;
+        this.modifierQuestion();
+    }
 
-	public String getType    () { return type    ; }
-	public String getIntitule() { return intitule; }
+    public void setIntitule(String intitule)
+    {
+        this.intitule = intitule;
+        this.modifierQuestion();
+    }
 
-	public int getNbPoints  () { return nbPoints  ; }
-	public int getTemps     () { return temps     ; }
-	public int getDifficulte() { return difficulte; }
+    public void setNbPoints(int nbPoints)
+    {
+        this.nbPoints = nbPoints;
+    }
 
-	public Notion getNotion() { return notion; }
+    public void setTemps(int temps)
+    {
+        this.temps = temps;
+        this.modifierQuestion();
+    }
 
-	public List<String> getComplements() { return complements; }
+    public void setDifficulte(int difficulte)
+    {
+        this.difficulte = difficulte;
+        this.modifierQuestion();
+    }
 
-	public IOption getOptions(int ind) { return ensOptions.get(ind);}
+    public void setComplements(ArrayList<String> complements)
+    {
+        this.complements = complements;
+    }
 
-	public List<IOption> getEnsOptions() { return ensOptions; }
+    public void setNotion(Notion notion)
+    {
+        this.notion = notion;
+        this.modifierQuestion();
+    }
 
-	public int getNumQuestion() {return this.numQuestion;}
+    /*
+     * +----------+
+     * | METHODES |
+     * +----------+
+     */
+    public void creerFichierQuestion()
+    {
+        Fichier tmp = new Fichier("lib/ressources/" + notion.getRessource().getNom() + "/" + notion.getNom() + "/");
+        tmp.ajouterFichier("question" + notion.getNbQuestion());
+        tmp.ajouterRtf("question" + notion.getNbQuestion() + "/question" + this.getNumQuestion());
+        tmp.ecrireQuestion("question" + notion.getNbQuestion() + "/question" + this.getNumQuestion(), this);
+    }
 
+    public boolean ajouterOption(IOption opt)
+    {
+        if (opt == null || this.ensOptions == null) return false;
+        this.ensOptions.add(opt);
+        return true;
+    }
 
-	/*
-	 *  +----------+
-	 *  | SETTEURS |
-	 *  +----------+
-	 */
-	public void setType    (String type    ) {
-		this.type     = type    ;
-		this.modifierQuestion();
-	}
-	public void setIntitule(String intitule) { 
-		this.intitule = intitule;
-		this.modifierQuestion();
-	}
+    public boolean supprimerOption(IOption opt)
+    {
+        return true; // Temporaire
+    }
 
-	public void setNbPoints  (int nbPoints  ) { 
-		this.nbPoints   = nbPoints  ; 
-	}
-	public void setTemps     (int temps     ) {
-		 this.temps      = temps     ;
-		 this.modifierQuestion(); 
-	}
-	public void setDifficulte(int difficulte) {
-		 this.difficulte = difficulte; 
-		 this.modifierQuestion();
-	}
-
-	public void setComplements(ArrayList<String> complements) {
-		 this.complements = complements; 
-	}
-
-	public void setNotion(Notion notion) { 
-		this.notion = notion;
-		this.modifierQuestion();
-	}
-
-
-
-	/*
-	 *  +----------+
-	 *  | METHODES |
-	 *  +----------+
-	 */
-
-	public void creerFichierQuestion()
-	{
-		Fichier tmp = new Fichier("lib/ressources/"+notion.getRessource().getNom()+"/"+notion.getNom()+"/");
-
-		tmp.ajouterFichier("question"+notion.getNbQuestion());
-		tmp.ajouterRtf("question"+notion.getNbQuestion()+"/"+"question"+this.getNumQuestion());
-		tmp.ecrireQuestion("question"+notion.getNbQuestion()+"/"+"question"+this.getNumQuestion(), this);
-	}
-
-	public boolean ajouterOption(IOption opt)
-	{
-		if (opt == null || this.ensOptions == null) return false;
-
-		this.ensOptions.add(opt);
-		return true;
-	}
-
-
-	public boolean supprimerOption(IOption opt)
-	{
-		return true; // Temporaire
-	}
-
-	public void modifierQuestion()
-	{
-		Fichier tmp = new Fichier("lib/ressources/"+notion.getRessource().getNom()+"/"+notion.getNom()+"/" +"question"+this.numQuestion+"/");
-		tmp.modifierQuestion("question"+this.getNumQuestion(), this);
-	}
+    public void modifierQuestion()
+    {
+        Fichier tmp = new Fichier("lib/ressources/" + notion.getRessource().getNom() + "/" + notion.getNom() + "/question" + this.numQuestion + "/");
+        tmp.modifierQuestion("question" + this.getNumQuestion(), this);
+    }
 }
