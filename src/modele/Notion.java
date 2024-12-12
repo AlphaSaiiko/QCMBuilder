370e16945e787f02a23bfe1e493f6c7e864875a1
Tatanelprr <ethan.lepareur@gmail.com>
2024-12-12 11:25:51 +0100

    Refactor Notion class for improved readability and consistency; update comments and method formatting

--- src/modele/Notion.java
+++ src/modele/Notion.java
@@ -3,114 +3,114 @@ package modele;
 import java.util.ArrayList;
 import java.util.List;
 
-import modele.Fichier;
-import modele.Question;
-import modele.Ressource;
-
 public class Notion
 {
-	/*
-	 *  +------------+
-	 *  | PARAMETRES |
-	 *  +------------+
-	 */ 
+    /*
+     * +------------+
+     * | PARAMETRES |
+     * +------------+
+     */ 
+    private List<Question> ensQuestions;
+    private String nom;
+    private Ressource ressource;
 
-	private List<Question> ensQuestions;
-
-	private String nom;
-
-	private Ressource ressource;
-
-	
-	/*
-	 *  +--------------+
-	 *  | CONSTRUCTEUR |
-	 *  +--------------+
-	 */
-
+    /*
+     * +--------------+
+     * | CONSTRUCTEUR |
+     * +--------------+
+     */
     public static Notion creerNotion(String nom, Ressource ressource)
-	{
-		Notion notion = Notion.trouverNotionParNom(nom, ressource);
-		if (notion == null)
-		{
-			notion = new Notion(nom, ressource);
-			System.out.println("Nouvelle notion créée avec le titre: " + nom + " pour la ressource: " + ressource.getNom());
-		}
-		else
-		{
-			System.out.println("La notion existe déjà dans la ressource: " + ressource.getNom());
-		}
-		return notion;
-	}
+    {
+        Notion notion = Notion.trouverNotionParNom(nom, ressource);
+        if (notion == null)
+        {
+            notion = new Notion(nom, ressource);
+            System.out.println("Nouvelle notion créée avec le titre: " + nom + " pour la ressource: " + ressource.getNom());
+        }
+        else
+        {
+            System.out.println("La notion existe déjà dans la ressource: " + ressource.getNom());
+        }
+        return notion;
+    }
 
-	private Notion(String nom, Ressource ressource)
-	{
-		this.nom = nom;
-		this.ressource = ressource;
-		this.creerFichierNotion();
-		this.ensQuestions = new ArrayList<Question>();
-		ressource.ajouterNotion(this);
-	}
+    private Notion(String nom, Ressource ressource)
+    {
+        this.nom = nom;
+        this.ressource = ressource;
+        this.creerFichierNotion();
+        this.ensQuestions = new ArrayList<Question>();
+        ressource.ajouterNotion(this);
+    }
 
-
-	/*
-	 *  +----------+
-	 *  | GETTEURS |
-	 *  +----------+
-	 */ 
-
-	public String getNom() { return nom; }
+    /*
+     * +---------+
+     * | GETTERS |
+     * +---------+
+     */
+    public String getNom()
+    {
+        return nom;
+    }
     
-	public Ressource getRessource() {return this.ressource;}
+    public Ressource getRessource()
+    {
+        return this.ressource;
+    }
 
-    public int getNbQuestion() { return this.ensQuestions.size(); }
+    public int getNbQuestion()
+    {
+        return this.ensQuestions.size();
+    }
 
-	public List<Question> getEnsQuestions() { return this.ensQuestions; }
+    public List<Question> getEnsQuestions()
+    {
+        return this.ensQuestions;
+    }
 
-	public Question getQuestion (int indice) { return this.ensQuestions.get(indice);}
+    public Question getQuestion(int indice)
+    {
+        return this.ensQuestions.get(indice);
+    }
 
+    /*
+     * +---------+
+     * | SETTERS |
+     * +---------+
+     */ 
+    public void setNom(String nom)
+    {
+        this.nom = nom;
+    }
 
-	/*
-	 *  +----------+
-	 *  | SETTEURS |
-	 *  +----------+
-	 */ 
+    public void setRessource(Ressource ressource)
+    {
+        this.ressource = ressource;
+    }
 
-	public void setNom(String nom) { this.nom = nom; }
+    /*
+     * +----------+
+     * | METHODES |
+     * +----------+
+     */
+    public void creerFichierNotion()
+    {
+        Fichier tmp = new Fichier("lib/ressources/" + ressource.getNom() + "/");
+        tmp.ajouterFichier(this.getNom());
+    }
+    
+    public void ajouterQuestion(Question question)
+    {
+        if (question != null)
+        {
+            this.ensQuestions.add(question);
+        }
+    }
 
-    public void setRessource(Ressource ressource) { this.ressource = ressource; }
-
-
-	/*
-	 *  +----------+
-	 *  | METHODES |
-	 *  +----------+
-	 */
-
-	public void creerFichierNotion()
-	{
-		Fichier tmp = new Fichier("lib/ressources/"+ressource.getNom()+"/");
-		tmp.ajouterFichier(this.getNom());
-	}
-	
-	public void ajouterQuestion(Question question)
-	{
-		if (question != null)
-		{
-			this.ensQuestions.add(question);
-		}
-	}
-
-	public void supprimerQuestion(Question question)
-	{
-		for (Question qst : this.ensQuestions)
-		{
-			if (question.equals(qst))
-			{
-				this.ensQuestions.remove(qst);
-			}
-		}
-	}
+    public void supprimerQuestion(Question question)
+    {
+        this.ensQuestions.removeIf(qst -> question.equals(qst));
+    }
 
     public static Notion trouverNotionParNom(String nom, Ressource ressource)
     {
