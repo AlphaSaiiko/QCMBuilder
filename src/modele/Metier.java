11b1643cb603a0dd681fa9becd78b7865f2415c5
Nahel <nahel.kochat@laposte.net>
2024-12-12 14:50:50 +0100

    get set

--- src/modele/Metier.java
+++ src/modele/Metier.java
@@ -8,32 +8,82 @@ import controleur.Controleur;
 public class Metier 
 {
 	
-    /*
-     * +------------+
-     * | Attributs  |
-     * +------------+
-     */
+	/*
+	 * +------------+
+	 * | Attributs  |
+	 * +------------+
+	 */
 
 	private Controleur controleur;
 
 	private static List<Evaluation> listEval;
 
-    private static List<Ressource>  listRessource;
+	private static List<Ressource>  listRessource;
 
 
-    /*
-     * +--------------+
-     * | Constructeur |
-     * +--------------+
-     */
+	/*
+	 * +--------------+
+	 * | Constructeur |
+	 * +--------------+
+	 */
 
 	public Metier(Controleur controleur)
 	{
 		this.controleur = controleur;
-		Metier.listEval      = new ArrayList<Evaluation>();
+		Metier.listEval	  = new ArrayList<Evaluation>();
 		Metier.listRessource = new ArrayList<Ressource>();
 	}
 
-    
+
+	/*
+	 * +---------+
+	 * | Getters |
+	 * +---------+
+	 */
+
+
+	public Controleur getControleur() 
+	{
+		return this.controleur;
+	}
+
+	public static List<Evaluation> getListEval() 
+	{
+		return Metier.listEval;
+	}
+
+	public static List<Ressource> getListRessource() 
+	{
+		return Metier.listRessource;
+	}
+
+
+
+
+	/*
+	 * +---------+
+	 * | Setters |
+	 * +---------+
+	 */
+	
+
+	public void setControleur(Controleur controleur) 
+	{
+		this.controleur = controleur;
+	}
+
+
+	public static void setListEval(List<Evaluation> listEval) 
+	{
+		Metier.listEval = listEval;
+	}
+
+
+	public static void setListRessource(List<Ressource> listRessource) 
+	{
+		Metier.listRessource = listRessource;
+	}
+
+
 
 }
