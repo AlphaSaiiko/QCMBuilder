b510f7444067de17ae269205ade25d8455a63541
Tatanelprr <ethan.lepareur@gmail.com>
2024-12-17 16:55:15 +0100

    Remove obsolete resource loading code from Controleur

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -36,48 +36,6 @@ public class Controleur
 	 */
 	public static void chargerRessourcesEtNotion()
 	{
-		/*String resourcesPath = "lib/ressources";
-		try (Stream<Path> paths = Files.walk(Paths.get(resourcesPath)))
-		{
-			paths.filter(Files::isDirectory).forEach(path ->
-			{
-				File dir = path.toFile();
-				File parent = dir.getParentFile();
-
-				// Si le répertoire actuel est une ressource
-				if (parent != null && parent.getName().equals("ressources"))
-				{
-					System.out.println("Ressource trouvée : " + dir.getName());
-					Ressource.creerRessource(dir.getName());
-				}
-				
-				// Si le répertoire parent est une ressource
-				else if (parent != null && parent.getParentFile() != null && parent.getParentFile().getName().equals("ressources"))
-				{
-					Ressource ressource = Ressource.creerRessource(parent.getName());
-					if (ressource == null)
-					{
-						System.out.println("Ressource non trouvée pour la notion, création d'une nouvelle ressource : " + parent.getName());
-						Ressource.creerRessource(parent.getName());
-						ressource = Ressource.creerRessource(parent.getName());
-					}
-
-					// Création de la notion
-					Notion notion = Notion.creerNotion(dir.getName(), ressource);
-					System.out.println("Notion créée : " + notion.getNom() + " pour la ressource : " + ressource.getNom());
-
-					Controleur.chargerQuestion(notion, dir);
-
-					// Ajout de la notion à la ressource
-					System.out.println("Notion ajoutée à la ressource : " + ressource.getNom());
-				}
-			});
-		}
-		catch (IOException e)
-		{
-			System.err.println("Erreur lors du chargement des ressources et des notions : " + e.getMessage());
-		}*/
-
 		try {
 			Scanner scRes = new Scanner(new FileInputStream("./Ressources.csv"));
 
