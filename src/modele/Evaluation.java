fd496d5e027b687104823b73cb6a0e24f35e69fa
Antonin Lopez <antoninlopez85@gmail.com>
2025-01-07 10:19:11 +0100

    evaluation clean

--- src/modele/Evaluation.java
+++ src/modele/Evaluation.java
@@ -12,14 +12,17 @@ public class Evaluation
 	 * +-----------+
 	 */
 
-	private Ressource       ressource    ;
-	private List<Notion>   listeNotions  ;
-	private List<Question> listeQuestions;
-	private String         lienEval      ; // Lien de l'évaluation à mettre sur l'intranet
-	private boolean			bChrono;
-	private int questionTf, questionF, questionM, questionD;
-	private float nbPoint;
-	private String chemin;
+	private boolean        bChrono        ;
+	private float          nbPoint        ;
+	private int            questionD      ;
+	private int            questionF      ;
+	private int            questionM      ;
+	private int            questionTf     ;
+	private List<Notion>   listeNotions   ;
+	private List<Question> listeQuestions ;
+	private Ressource      ressource      ;
+	private String         chemin         ;
+	private String         lienEval       ; // Lien de l'évaluation à mettre sur l'intranet
 
 
 
@@ -32,13 +35,16 @@ public class Evaluation
 
 	public Evaluation(Ressource ressource, boolean bChrono, String chemin)
 	{
-		this.ressource	    = ressource                ;
+		this.bChrono        = bChrono                  ;
+		this.nbPoint        = 0                        ;
+		this.questionD      = 0                        ;
+		this.questionF      = 0                        ;
+		this.questionM      = 0                        ;
+		this.questionTf     = 0                        ;
 		this.listeNotions   = new ArrayList<Notion>  ();
 		this.listeQuestions = new ArrayList<Question>();
-		this.bChrono = bChrono;
-		this.questionTf = this.questionF = this.questionM = this.questionD = 0;
-		this.nbPoint = 0;
-		this.chemin = chemin;
+		this.ressource      = ressource                ;
+		this.chemin         = chemin                   ;
 	}
 
 
@@ -50,18 +56,18 @@ public class Evaluation
 	 * +----------+
 	 */
 
-	public Ressource      getRessource     () { return ressource     ; }
-	public List<Notion>   getListeNotions  () { return listeNotions  ; }
-	public List<Question> getListeQuestions() { return listeQuestions; }
-	public String         getLienEval      () { return lienEval      ; }
-	public boolean getChrono() { return this.bChrono;}
-	public int getQuestionTresFacile() { return this.questionTf;}
-	public int getQuestionFacile() { return this.questionF;}
-	public int getQuestionMoyen() { return this.questionM;}
-	public int getQuestionDifficile() { return this.questionD;}
-	public int getNbQuestion() { return this.questionTf+this.questionF+this.questionM+this.questionD;}
-	public float getNbPoints() {return this.nbPoint;}
-	public String getChemin() {return this.chemin;}
+	public boolean        getChrono            () { return this.bChrono                                                       ; }
+	public float          getNbPoints          () { return this.nbPoint                                                       ; }
+	public int            getNbQuestion        () { return this.questionTf + this.questionF + this.questionM + this.questionD ; }
+	public int            getQuestionDifficile () { return this.questionD                                                     ; }
+	public int            getQuestionFacile    () { return this.questionF                                                     ; }
+	public int            getQuestionMoyen     () { return this.questionM                                                     ; }
+	public int            getQuestionTresFacile() { return this.questionTf                                                    ; }
+	public List<Notion>   getListeNotions      () { return this.listeNotions                                                  ; }
+	public List<Question> getListeQuestions    () { return this.listeQuestions                                                ; }
+	public Ressource      getRessource         () { return this.ressource                                                     ; }
+	public String         getChemin            () { return this.chemin                                                        ; }
+	public String         getLienEval          () { return this.lienEval                                                      ; }
 
 
 
@@ -84,13 +90,6 @@ public class Evaluation
 	 * +----------+
 	 */
 
-	/**
-	 * Ajoute une question à la liste des questions si elle n'est pas nulle.
-	 *
-	 * @param question La question à ajouter à la liste.
-	 *                 Elle ne doit pas être nulle.
-	 * @return true si la question a été ajoutée avec succès, false sinon.
-	 */
 	public boolean ajouterQuestion(Question question)
 	{
 		if (this.listeQuestions != null && question != null)
@@ -103,12 +102,6 @@ public class Evaluation
 
 
 
-	/**
-	 * Ajoute une notion à la liste des notions si elle n'est pas nulle.
-	 *
-	 * @param notion La notion à ajouter à la liste.
-	 *               Elle ne doit pas être nulle.
-	 */
 	public void ajouterNotion(Notion notion)
 	{
 		if (notion != null && this.listeNotions != null)
@@ -116,26 +109,30 @@ public class Evaluation
 	}
 
 
+
 	public String toString()
 	{
-		StringBuilder notionsStr = new StringBuilder();
+		StringBuilder stringNotions = new StringBuilder();
+
 		for (Notion notion : listeNotions)
-		{
-			notionsStr.append(notion.getNom()).append(", ");
-		}
-		// Supprimer la dernière virgule et l'espace
-		if (notionsStr.length() > 0)
-		{
-			notionsStr.setLength(notionsStr.length() - 2);
-		}
-		return "Evaluation{" + "ressource=" + ressource.getNom() + ", notions=[" + notionsStr.toString() + "]"
-				+ ", lienEval='" + lienEval + '\'' + '}';
+			stringNotions.append(notion.getNom()).append(", ");
+
+		if (stringNotions.length() > 0)
+			stringNotions.setLength(stringNotions.length() - 2);
+			
+		return "Evaluation{"  +
+			   "ressource="   + ressource.getNom()       +
+			   ", notions=["  + stringNotions.toString() + "]"  +
+			   ", lienEval='" + lienEval                 + '\'' + '}';
 	}
 
+
+	
 	public void recupererQuestion(Notion notion, int tf, int f, int m, int d)
 	{
-		List<Question> tmpLst = new ArrayList<Question>();
+		List<Question> tmpLst      = new ArrayList<Question>();
 		List<Question> lstQuestion = new ArrayList<Question>();
+
 		lstQuestion = notion.getListeQuestions();
 
 		Collections.shuffle(lstQuestion);
@@ -145,39 +142,38 @@ public class Evaluation
 			if (question.getDifficulte() == 1 && tf>0)
 			{
 				tmpLst.add(question);
-				tf = tf-1;
+				tf = tf - 1;
 				this.questionTf++;
 				this.nbPoint += question.getNbPoints();
 			}
 			if (question.getDifficulte() == 2 && f>0)
 			{
 				tmpLst.add(question);
-				f = f-1;
+				f = f - 1;
 				this.questionF++;
 				this.nbPoint += question.getNbPoints();
 			}
 			if (question.getDifficulte() == 3 && m>0)
 			{
 				tmpLst.add(question);
-				m = m-1;
+				m = m - 1;
 				this.questionM++;
 				this.nbPoint += question.getNbPoints();
 			}
 			if (question.getDifficulte() == 4 && d>0)
 			{
 				tmpLst.add(question);
-				d = d-1;
+				d = d - 1;
 				this.questionD++;
 				this.nbPoint += question.getNbPoints();
 			}
 		}
 
 		for (Question question : tmpLst)
-		{
 			this.listeQuestions.add(question);
-		}
 
 		this.listeNotions.add(notion);
+
 		Collections.shuffle(this.listeQuestions);
 	}
 }
